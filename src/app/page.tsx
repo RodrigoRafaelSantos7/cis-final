c70cd0239abc41348e375e4ca0f0021502c08b8d
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-02-13 10:36:32 +0000

    Add Lenis smooth scrolling and zoom parallax image gallery to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Link from "next/link";
-
+"use client";
 import styles from "./index.module.css";
+import Lenis from "lenis";
+import { useEffect } from "react";
+import ZoomParallax from "~/components/ZoomParallax";
 
 export default function Home() {
-  return <main></main>;
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <ZoomParallax />
+    </main>
+  );
 }
